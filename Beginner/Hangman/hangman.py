f31a68c78a71fe28fb15dec53b8e8d88233cfe89
Eamateli <e.a.mateli@gmail.com>
2026-05-30 00:59:42 +0300

    Update .gitignore

--- Beginner/Hangman/hangman.py
+++ Beginner/Hangman/hangman.py
@@ -8,18 +8,29 @@ word_display = [ "_" for _ in word]
 
 wrong_guesses = 0
 
+guessed_letters = []
+
 while wrong_guesses < 6:
     print("Word:" + " ".join(word_display))
+    print("Guessed: " + ", ".join(guessed_letters))
     if "_" not in word_display:
         print(f"You won! The word was: {word}")
-        break
+        break   
     guess = input("Guess a letter: ").lower()
-
+    if len(guess) != 1:
+        print("Please enter exacly one letter.")
+        continue
+    if not guess.isalpha():
+        print("Please enter a letter (a-z).")
+        continue
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+    guessed_letters.append(guess)
     if guess in word:
         for i in range(len(word)):
             if word[i]==guess:
-                word_display[i]=guess
-                
+                word_display[i]=guess               
     else:
         wrong_guesses += 1
         print(f"Nope. Wrong guesses: {wrong_guesses}/6")

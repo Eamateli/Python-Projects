a756b9ba47ae9145dcc911e6355aed3b2b1f260e
Eamateli <e.a.mateli@gmail.com>
2026-05-31 01:55:56 +0300

    Refactor

--- Beginner/Hangman/hangman.py
+++ Beginner/Hangman/hangman.py
@@ -2,38 +2,40 @@ import random
 
 words = ["python", "computer", "keyboard", "function", "developer"]
 
-word = random.choice(words)
 
-word_display = [ "_" for _ in word]
+def play_round():
 
-wrong_guesses = 0
+    word = random.choice(words)
+    word_display = [ "_" for _ in word]
+    wrong_guesses = 0
+    guessed_letters = []
 
-guessed_letters = []
+    while wrong_guesses < 6:
+        print("Word:" + " ".join(word_display))
+        print("Guessed: " + ", ".join(guessed_letters))
+        if "_" not in word_display:
+            print(f"You won! The word was: {word}")
+            break   
+        guess = input("Guess a letter: ").lower()
+        if len(guess) != 1:
+            print("Please enter exacly one letter.")
+            continue
+        if not guess.isalpha():
+            print("Please enter a letter (a-z).")
+            continue
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+        guessed_letters.append(guess)
+        if guess in word:
+            for i in range(len(word)):
+                if word[i]==guess:
+                    word_display[i]=guess               
+        else:
+            wrong_guesses += 1
+            print(f"Nope. Wrong guesses: {wrong_guesses}/6")
 
-while wrong_guesses < 6:
-    print("Word:" + " ".join(word_display))
-    print("Guessed: " + ", ".join(guessed_letters))
-    if "_" not in word_display:
-        print(f"You won! The word was: {word}")
-        break   
-    guess = input("Guess a letter: ").lower()
-    if len(guess) != 1:
-        print("Please enter exacly one letter.")
-        continue
-    if not guess.isalpha():
-        print("Please enter a letter (a-z).")
-        continue
-    if guess in guessed_letters:
-        print("You already guessed that letter.")
-        continue
-    guessed_letters.append(guess)
-    if guess in word:
-        for i in range(len(word)):
-            if word[i]==guess:
-                word_display[i]=guess               
-    else:
-        wrong_guesses += 1
-        print(f"Nope. Wrong guesses: {wrong_guesses}/6")
+    if wrong_guesses == 6:
+        print(f"Game over! The word was: {word}")
 
-if wrong_guesses == 6:
-    print(f"Game over! The word was: {word}")
+play_round()
